4fb328fc0d9c1d745e4df1aaf13f46cf3586049a
Mücahit Şahin <mucahitsahin786@gmail.com>
2022-01-06 16:21:15 +0300

    make it better

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use("/user", userRouter);
 app.use("/post", postRouter);
 app.use("/agenda", agendaRouter);
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("server is running");
 });
 
