cfe90d7bc8fec37435c498babacd10f444c745b5
Mücahit Şahin <mucahitsahin786@gmail.com>
2022-05-29 21:49:03 +0300

    Created getYesterdayLikedPosts

--- controllers/agenda.js
+++ controllers/agenda.js
@@ -65,3 +65,35 @@ export const getYesterdayAgenda = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+// yesterday's most liked posts
+export const getYesterdayLikedPosts = async (req, res) => {
+  try {
+    const response = [];
+    let date = new Date().getTime();
+    const LIMIT = 10;
+    const posts = await Post.find().sort({ changedAt: -1 }).limit(LIMIT);
+    posts
+      .filter((post) => {
+        return (
+          post.changedAt.toISOString().slice(0, 10) ==
+          new Date(date - 86400000).toISOString().slice(0, 10)
+        );
+      })
+      .sort((a, b) => {
+        return b.comments[0].likes.length > a.comments[0].likes.length;
+      })
+      .map((post) => {
+        response.push({
+          id: post._id,
+          title: post.title,
+          slug: post.slug,
+          changedAt: post.changedAt,
+          yesterdayLikedCount: post.comments[0].likes.length,
+        });
+      });
+    res.json(response);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
--- routes/agenda.js
+++ routes/agenda.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { getAgenda } from "../controllers/agenda.js";
+import { getAgenda, getYesterdayLikedPosts } from "../controllers/agenda.js";
 
 const router = express.Router();
 
 router.get("/", getAgenda);
+router.get("/mostliked", getYesterdayLikedPosts);
 
 export default router;
